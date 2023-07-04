8bc4aabd2f92c9c805e873e5c6d43e727a9d44b0
KagontleBooysen <k.booysen@alustudent.com>
2023-07-03 20:06:42 -0700

    6-set.js

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,3 +1,6 @@
-import setFromArray from "./6-set.js";
-
-console.log(setFromArray([12, 32, 15, 78, 98, 15]));
+export default function setFromArray(arr) {
+  const s = new Set();
+  // eslint-disable-next-line
+    arr.map(val => { s.add(val) });
+  return s;
+};
